8b9c6d08e4852a88befd6a7fcf48c79215a2d46d
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-09-26 13:35:34 +0300

    Updated if elses for form

--- project1/javascript/functions.js
+++ project1/javascript/functions.js
@@ -4,7 +4,28 @@ function listPrint() {
   var y = document.getElementById('t2').value;
   var z = document.getElementById('t3').value;
 
-  if( z == '' && y == '') {
+//IF THE FORM IS SUBMITTED EMPTY
+  if (z == '' && x == '' && y == '') {
+    // Get the modal
+    var modal = document.getElementById("mod");
+    modal.style.display = "block";
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
+//-------------------------------------------------//
+
+//IF ONLY ONE ELEMENT IS ADDED-----------------------//
+
+  else if( z == '' && y == '') {
     var listElement = document.createElement("li");
     var listContent = document.createTextNode(x);
     listElement.appendChild(listContent);
@@ -18,14 +39,68 @@ function listPrint() {
     document.getElementById("list").appendChild(listElement);
   }
 
-  else if( x == '') {
+  else if( x == '' && y == '') {
     var listElement = document.createElement("li");
     var listContent = document.createTextNode(z);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+  }
+
+//--------------------------------------------------------------//
+
+//IF TWO ELEMNTS ARE ADDED-------------------------------------//
+
+  else if( x == '') {
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(y);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(z);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+  }
+
+  else if( z == '') {
+  var listElement = document.createElement("li");
+  var listContent = document.createTextNode(x);
+  listElement.appendChild(listContent);
+  document.getElementById("list").appendChild(listElement);
+
+  var listElement = document.createElement("li");
+  var listContent = document.createTextNode(y);
+  listElement.appendChild(listContent);
+  document.getElementById("list").appendChild(listElement);
+  }
+
+  else if( y == '') {
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(x);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(z);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+  }
+  //--------------------------------------------------------//
+
+  //IF ALL 3 ARE ADDED
+  else {
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(x);
     listElement.appendChild(listContent);
     document.getElementById("list").appendChild(listElement);
 
     var listElement = document.createElement("li");
     var listContent = document.createTextNode(y);
+    listElement.appendChild(listContent);
+    document.getElementById("list").appendChild(listElement);
+
+    var listElement = document.createElement("li");
+    var listContent = document.createTextNode(z);
     listElement.appendChild(listContent);
     document.getElementById("list").appendChild(listElement);
   }

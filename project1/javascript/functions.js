a26ab519277cc527d5066db62f78f870d1a06c36
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-05 18:37:24 +0300

    okok

--- project1/javascript/functions.js
+++ project1/javascript/functions.js
@@ -26,6 +26,7 @@ function listPrint() {
         else {
           for(var i = 0; i < tasks.length-1;i++) { //tasks.length-1 because 'submit' considers itself as input, and we dont want to print that to the site
             var listElement = document.createElement("li"); //Creating an 'li' element
+            listElement.onclick = function(e) { this.parentNode.removeChild(this) };
             var listContent = document.createTextNode(tasks[i].value); //Creating a text node, that can be given to the 'li' element
               if(tasks[i].value == ''){
                 console.log('Empty values are not printed'); //if 'tasks[i].value' is empty, it is not printed to the user
@@ -39,32 +40,25 @@ function listPrint() {
         document.getElementById('t1').value = '';
         document.getElementById('t2').value = ''; //REMOVE COMMENT TAG IF WE WANT THE FORM TO EMPTY ITSELF AFTER SUBMISSION
         document.getElementById('t3').value = '';
-        hideBtn(); //Initializes the button to the site
+        hideInfo();
   }
   else {
     alert("The list can only hold 6 items in it");
   }
 }
 
-function deleteChild() {
-  var remove = document.getElementById('list');
-  remove.removeChild(remove.firstChild);
-  hideBtn(); //After every deletion, the button's display condition is checked
-}
-
-function hideBtn() {
-  if(document.getElementsByTagName('li').length <= 0){ //If there are no 'li' elements on the site, button is hidden
-    document.getElementById('dlt').style.display = 'none'; // when 'deleteChild()' removes the final element, the button hides itself
-  }
-  else {
-     document.getElementById('dlt').style.display = 'block'; //As form submission adds 'li' element to the site, it is always shown after the first submission
-  }
-}
-
 function show(){
   document.getElementById('who').style.display = 'inline';
 }
-
 function hide() {
   document.getElementById('who').style.display = 'none'; //On mouseout, the text is hidden again
+}
+
+function hideInfo() {
+  if(document.getElementsByTagName('li').length <= 0) {
+    document.getElementById('info').style.display = 'none';
+  }
+  else {
+    document.getElementById('info').style.display = 'inline';
+  }
 }

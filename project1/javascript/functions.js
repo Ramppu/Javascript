871271a35adc04b9ab0698ffa35a4b43ce364e6a
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-05 22:15:02 +0300

    ko

--- project1/javascript/functions.js
+++ project1/javascript/functions.js
@@ -1,5 +1,4 @@
 function listPrint() {
-
   var x = document.getElementById('t1').value;
   var y = document.getElementById('t2').value;// X, Z and Y VALUES ARE USED FOR THE MODAL GENERATION
   var z = document.getElementById('t3').value;
@@ -18,7 +17,7 @@ function listPrint() {
           }
 
           window.onclick = function(event) {
-            if (event.target == modal) { // Clicking anywhere on the window hides the modal too
+            if (event.target == modal) { //Clicking anywhere on the window hides the modal too
               modal.style.display = "none";
             }
           }
@@ -26,7 +25,7 @@ function listPrint() {
         else {
           for(var i = 0; i < tasks.length-1;i++) { //tasks.length-1 because 'submit' considers itself as input, and we dont want to print that to the site
             var listElement = document.createElement("li"); //Creating an 'li' element
-            listElement.onclick = function(e) { this.parentNode.removeChild(this) };
+            listElement.onclick = function(e) { this.parentNode.removeChild(this)}; //Creating a function for every 'li' element, that removes it by mouseclick
             var listContent = document.createTextNode(tasks[i].value); //Creating a text node, that can be given to the 'li' element
               if(tasks[i].value == ''){
                 console.log('Empty values are not printed'); //if 'tasks[i].value' is empty, it is not printed to the user
@@ -38,27 +37,29 @@ function listPrint() {
           }
         }
         document.getElementById('t1').value = '';
-        document.getElementById('t2').value = ''; //REMOVE COMMENT TAG IF WE WANT THE FORM TO EMPTY ITSELF AFTER SUBMISSION
+        document.getElementById('t2').value = ''; //Used to empty the form after submission
         document.getElementById('t3').value = '';
-        hideInfo();
+        hideInfo(); //Brings out the 'info' div
   }
   else {
     alert("The list can only hold 6 items in it");
   }
 }
 
+//  NOT SURE IF THIS WILL EVER BE USED------------------------------------------------------------
 function show(){
   document.getElementById('who').style.display = 'inline';
 }
 function hide() {
   document.getElementById('who').style.display = 'none'; //On mouseout, the text is hidden again
 }
+//-------------------------------------------------------------------------------------------------
 
 function hideInfo() {
-  if(document.getElementsByTagName('li').length <= 0) {
-    document.getElementById('info').style.display = 'none';
+  if(document.getElementsByTagName('li').length <= 0) { // if there are no 'li' elements on the website, 'info' div is hidden
+    document.getElementById('info').style.opacity = '0.0';
   }
   else {
-    document.getElementById('info').style.display = 'inline';
+    document.getElementById('info').style.opacity = '1.0';
   }
 }

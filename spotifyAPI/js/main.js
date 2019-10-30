608e8cf40c658bf86117ff7a602e2850ffa24300
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-30 13:50:27 +0200

    dasd

--- spotifyAPI/js/main.js
+++ spotifyAPI/js/main.js
@@ -1,16 +1,16 @@
-var url = "url here";
-var auth = "auth here";
+var url = "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist";
+var auth = "BQAgEveRY72-yvZXv-kugJGDz6XZTJ9eFY_Wph0Jb-bgmKzXUD9MM4ITY-zho0R4nl9gUQ4-v5w82mp-CEYozUkdOMADNkBVILfCfVvtzWy83UEdfG7kSEk303xTbh5M-LSG5C_1kfbGVlC6-LGCUHPyV1ZyQ_Jx_hY";
 
 function searchSpotify() {
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","https://api.spotify.com/v1/albums/{id}",true);
+  xmlhttp.open("GET",url,true);
   xmlhttp.setRequestHeader("Content-Type", "application/json");
-  xmlhttp.setRequestHeader('Authorization', auth);
+  xmlhttp.setRequestHeader("Authorization", auth);
   xmlhttp.send();
 
   xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-       alert(xmlhttp.responseText);
+       console.log(xmlhttp.responseText);
     }
   }
 }

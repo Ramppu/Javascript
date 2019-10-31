96915f95726be3bb9114d22d66e811a1017c67b8
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-10-31 23:38:53 +0200

    workin

--- spotifyAPI/js/main.js
+++ spotifyAPI/js/main.js
@@ -1,21 +1,23 @@
 
 
 function searchSpotify() {
-  window.location.replace("https://accounts.spotify.com/authorize?client_id=c7e183fe68ed459e95e67fde31ce89b8&redirect_uri=https://projectra.netlify.com/&scope=user-read-private%20user-read-email&response_type=token&state=123");
+  window.location.replace("https://accounts.spotify.com/authorize?client_id=c7e183fe68ed459e95e67fde31ce89b8&redirect_uri=https://nostalgic-edison-5f972e.netlify.com&scope=user-read-private%20user-read-email&response_type=token&state=123");
 }
+function search() {
+  var url = document.URL,
+  access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+  var q = document.getElementById('artist').value;
+  var auth = `Bearer ${access_token}`;
+  var searchUrl = `https://api.spotify.com/v1/search?q=${q}&type=artist`;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET",searchUrl,true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.setRequestHeader("Authorization", auth);
+  xmlhttp.send();
 
-function login() {
-  console.log("lol");
-    main.get('/login', function(req, res) {
-    var scopes = 'user-read-private user-read-email';
-    res.redirect('https://accounts.spotify.com/authorize' +
-  '?response_type=code' +
-  '&client_id=' + my_client_id +
-  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  '&redirect_uri=' + encodeURIComponent(redirect_uri));
-});
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      console.log(xmlhttp.responseText);
+   }
+ }
 }
-    //input field id="paska"
-    //var url = document.getElementById('paska').value;
-    //<p id="kulli">iqj092093ieweoiqwqe3gpoiwgwgojg</p>
-    //var auth = document.getElementById('kulli').innerHTML;/.value;

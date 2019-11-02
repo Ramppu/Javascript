a981f945eaa5094454ff25f3248c191215e5867c
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-11-02 18:54:00 +0200

    pepeOK

--- spotifyAPI/js/main.js
+++ spotifyAPI/js/main.js
@@ -1,5 +1,3 @@
-
-
 function grantToken() {
   window.location.replace("https://accounts.spotify.com/authorize?client_id=c7e183fe68ed459e95e67fde31ce89b8&redirect_uri=https://nostalgic-edison-5f972e.netlify.com&scope=user-read-private%20user-read-email&response_type=token&state=123");
 }

47a9c6226d94113a8eb0943d846f061c4c3605b2
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-11-04 10:30:31 +0200

    We out here doing work

--- spotifyAPI/js/main.js
+++ spotifyAPI/js/main.js
@@ -1,38 +1,51 @@
 function grantToken() {
   window.location.replace("https://accounts.spotify.com/authorize?client_id=c7e183fe68ed459e95e67fde31ce89b8&redirect_uri=https://nostalgic-edison-5f972e.netlify.com&scope=user-read-private%20user-read-email&response_type=token&state=123");
-}
+} //Grants access token so searches can be made
+
 function search() {
   //document.getElementById('picture').removeChild(document.getElementById('picture').firstChild);
   //document.getElementById('info').InnerHTML = "";
   //document.getElementById('link').InnerHTML = "";
 
   var url = document.URL,
-  access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
-  var q = document.getElementById('query').value;
-  var type = document.getElementById('type').value;
-  var auth = `Bearer ${access_token}`;
-  var searchUrl = `https://api.spotify.com/v1/search?q=${q}&type=${type}`;
+  access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]; //Pulls access token out of the url
+  var q = document.getElementById('query').value; //The search query from form
+  var type = document.getElementById('type').value; //The search type from form
+  var auth = `Bearer ${access_token}`; //Access token for the AJAX request
+  var searchUrl = `https://api.spotify.com/v1/search?q=${q}&type=${type}`; //Final search url for the AJAX request
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET",searchUrl,true);
   xmlhttp.setRequestHeader("Content-Type", "application/json");
   xmlhttp.setRequestHeader("Authorization", auth);
-  xmlhttp.send();
+  xmlhttp.send(); //Completed AJAX request with all the required information
 
   xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
       console.log(xmlhttp.responseText);
        var data = JSON.parse(xmlhttp.responseText);
-       var img = document.createElement('img'); // Picture
-       var text = document.createElement('p');
-       var info = document.createElement('p');
+       var img = document.createElement('img'); //Img element for the picture div
+       var text = document.createElement('p'); //P element for the link div
+       var info = document.createElement('p'); //P element for the info div
 
-       if (document.getElementById('type').value == 'artist') {
-         img.src = data.artists.items[0].images[0].url; // artist picture
+       //Respone given by the AJAX Request is different based on the search type
+       if (document.getElementById('type').value == 'artist') { //If user is searching for artists
+         img.src = data.artists.items[0].images[1].url; // artist picture
          var link = document.createTextNode("Go To The Artist");
-         text.onclick = function(e) {window.open(data.albums.items[0].external_urls.spotify,'_blank')};
+         text.onclick = function(e) {window.open(data.artists.items[0].external_urls.spotify,'_blank')}; //Onclick function for the previous text node that takes the user to the artist page
+         text.append(link);
+         text.style.cursor = "pointer";
+
+         var artist = document.createTextNode("Artist: " + data.artists.items[0].name);
+         var followers = document.createTextNode("Followers: " + data.artists.items[0].followers.total);
+         var genre = document.createTextNode("Genre: " + data.artists.items[0].genres[0]);
+         info.append(artist);
+         info.append(document.createElement('br'));
+         info.append(genre);
+         info.append(document.createElement('br'));
+         info.append(followers);
          //MORE ARTIST SHIT
        }
-       if(document.getElementById('type').value == 'album') {
+       if(document.getElementById('type').value == 'album') { //If user is searching for albums
          img.src = data.albums.items[0].images[1].url; //album picture
          var link = document.createTextNode("Go To The Album");
          text.onclick = function(e) {window.open(data.albums.items[0].external_urls.spotify,'_blank')};
@@ -51,7 +64,7 @@ function search() {
          info.append(count);
          //MORE ALBUM SHIT
        }
-       if(document.getElementById('type').value == 'track') {
+       if(document.getElementById('type').value == 'track') { //If user is searching for songs
         img.src = data.tracks.items[0].album.images[1].url; // Album pic via song
         var album = document.createTextNode("A Song Within: " + data.tracks.items[0].album.name);
         var release = document.createTextNode("Album's Release Date: " + data.tracks.items[0].album.release_date);//MORE SONG SHIT
@@ -67,14 +80,13 @@ function search() {
         text.style.cursor = "pointer";
         text.append(link);
       }
-      document.getElementById('picture').append(img);
+      $("#picture").html(img);//document.getElementById('picture').innerHTML = `${img}`;
 
-      document.getElementById('info').append(info);
-      document.getElementById('info').style.height = "200px";
-      document.getElementById('info').style.border = "solid #1DB954";
-      document.getElementById('info').style.borderRadius = "25px";
+      $("#info").html(info);//document.getElementById('info').innerHTML = `${info}`;
 
-      document.getElementById('link').append(text);
+      document.getElementById("info").classList.add("info"); 
+
+      $("#link").html(text);//document.getElementById('link').innerHTML = `${text}`;
    }
  }
 }
